d67ffa498da2f536e07767e58002a36d24c5f3cb
Neftalem <dneftalem@gmail.com>
2026-01-02 19:12:50 +0300

    create rigister logic

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,3 +1,27 @@
 
 const db = require("../config/db");
 const bcrypt = require("bcrypt");
+
+exports.registerUser = async (req, res) => {
+
+    const { email, password, first_name, last_name, nickname, phone_numer, identity } = req.body;
+    const hashed = await bcrypt.hash(password, 10);
+    const sql = "INSERT INTO users (email, password, first_name, last_name, nickname, phone_numer, identity) VALUES (?, ?, ?, ?, ?, ?, ?)"
+
+    db.query(sql [email, password, first_name, last_name, nickname, phone_numer, identity], (err, result) => {
+
+        if(err) {
+
+            if(err.code === "ER_DUP_ENTRY") {
+                return res.status(409).json({ message: "Email already exists"});
+            }
+
+            return res.status(500).json({ error: err.message});
+
+        }
+
+        res.json({message: "User registered successfully"})
+
+    })
+
+}
