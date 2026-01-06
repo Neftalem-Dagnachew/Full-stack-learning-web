b0eb2628b72ce31f436b82368be9bc344e8ac173
Neftalem <dneftalem@gmail.com>
2026-01-06 10:28:44 +0300

    add logic auth

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,1 +1,21 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECTET = "b3f9cA7xP!2kL8mZqR#WvD1E@6yN0H4sJtUo";
 
+module.exports = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    if(!authHeader) return res.status(401).json({ message: "No token provide"});
+
+    const token = authHeader.split(" ")[1];
+    if(!token) return res.status(401).json({ message: "Invalid token"});
+
+    try {
+
+        const decode = jwt.verify(token, JWT_SECTET);
+        req.user = decode;
+        next()
+
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token"});
+    }
+
+}
