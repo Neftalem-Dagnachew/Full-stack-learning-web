a85fe5d43f5789a50129ad59d8d63f6ba1862aab
Neftalem <dneftalem@gmail.com>
2026-01-06 10:54:11 +0300

    conected with router

--- Backend/routes/userRouters.js
+++ Backend/routes/userRouters.js
@@ -2,11 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const auth = require("../middleware/auth")
 
+// get user
+router.get("/get", userController.getUsers)
+router.get("/Profile", auth, userController.getMe)
 
+// Register & login
 router.post("/register", userController.registerUsers);
 router.post("/login", userController.loginUsers)
-router.get("/get", userController.getUsers)
+
 
 
 
