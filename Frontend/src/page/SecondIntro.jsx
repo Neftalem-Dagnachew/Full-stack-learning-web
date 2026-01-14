ab50d2a98fb7a5d911b2008422dbe8e6a1f02685
Neftalem <dneftalem@gmail.com>
2026-01-14 11:01:14 +0300

    install fontAwesome and use it

--- Frontend/src/page/SecondIntro.jsx
+++ Frontend/src/page/SecondIntro.jsx
@@ -5,28 +5,28 @@ function SecondIntro() {
 
     return(
         <>
-            <div class="second_content_container d-flex justify-content-center">
+            <div className="second_content_container d-flex justify-content-center">
 
-            <div class="all_second_container d-flex justify-content-start">
+            <div className="all_second_container d-flex justify-content-start">
 
-                <div class="sec_img_container position-relative d-flex">
+                <div className="sec_img_container position-relative d-flex">
 
-                   <img class="sec_img" src="IMG/STU2.png" alt="" />
+                   <img className="sec_img" src="IMG/STU2.png" alt="" />
 
-                    {/* <!-- <div class="d-flex align-items-center"> --> */}
+                    {/* <!-- <div className="d-flex align-items-center"> --> */}
 
-                        {/* <!-- <div class="img_card_container"> --> */}
+                        {/* <!-- <div className="img_card_container"> --> */}
                             
-                            <div class="first_card">
+                            <div className="first_card">
 
-                                <div class="d-flex align-items-center gap-3">
+                                <div className="d-flex align-items-center gap-3">
 
-                                    <img class="card_img" src="IMG/seq_wine1.png" alt="" />
+                                    <img className="card_img" src="IMG/seq_wine1.png" alt="" />
 
                                     <div>
 
-                                        <h6 class="poppins-semibold">Interesting online course</h6>
-                                        <p class="m-0 poppins-extralight">It's the same as what you learn in school.</p>
+                                        <h6 className="poppins-semibold">Interesting online course</h6>
+                                        <p className="m-0 poppins-extralight">It's the same as what you learn in school.</p>
 
                                     </div>
 
@@ -34,16 +34,16 @@ function SecondIntro() {
 
                             </div>
 
-                            <div class="second_card">
+                            <div className="second_card">
 
-                                <div class="d-flex align-items-center gap-3">
+                                <div className="d-flex align-items-center gap-3">
 
-                                    <img class="card_img" src="IMG/star_seq.png" alt="" />
+                                    <img className="card_img" src="IMG/star_seq.png" alt="" />
 
                                     <div>
 
-                                        <h6 class="poppins-semibold">Qualified teachers</h6>
-                                        <p class="m-0 poppins-extralight">Qualified and experienced teachers.</p>
+                                        <h6 className="poppins-semibold">Qualified teachers</h6>
+                                        <p className="m-0 poppins-extralight">Qualified and experienced teachers.</p>
 
                                     </div>
 
@@ -51,16 +51,16 @@ function SecondIntro() {
 
                             </div>
 
-                            <div class="third_card">
+                            <div className="third_card">
 
-                                <div class="d-flex align-items-center gap-3">
+                                <div className="d-flex align-items-center gap-3">
 
-                                    <img class="card_img" src="IMG/student_seq.png" alt="" />
+                                    <img className="card_img" src="IMG/student_seq.png" alt="" />
 
                                     <div>
 
-                                        <h6 class="poppins-semibold">100,000 students</h6>
-                                        <p class="m-0 poppins-extralight">Over 100,000 students are using our YouTube channel.</p>
+                                        <h6 className="poppins-semibold">100,000 students</h6>
+                                        <p className="m-0 poppins-extralight">Over 100,000 students are using our YouTube channel.</p>
 
                                     </div>
 
@@ -74,57 +74,57 @@ function SecondIntro() {
 
                 </div>
 
-                <div class="d-flex align-items-center">
+                <div className="d-flex align-items-center">
 
-                    <div class="sec_allcontent_container">
+                    <div className="sec_allcontent_container">
 
-                        <div class="sec_all_text_container">
+                        <div className="sec_all_text_container">
 
-                            <div class="pb-4">
+                            <div className="pb-4">
 
-                                <h3 class="sec_main_hade poppins-medium m-0">Study with us!</h3>
-
-                            </div>
-
-                            <div class="pb-4">
-
-                                <h2 class="sec_use_text poppins-bold m-0">Why is Saqima useful?</h2>
+                                <h3 className="sec_main_hade poppins-medium m-0">Study with us!</h3>
 
                             </div>
 
-                            <div class="pb-5">
+                            <div className="pb-4">
 
-                                <p class="m-0 poppins-extralight">Learn visually by watching our videos, understand the lesson by doing questions, and assess your skills by taking tests!</p>
-
-                            </div>
-
-                            <div class="pb-4">
-
-                                <p class="m-0 poppins-light"><i class="fa-solid fa-check chakemark"></i>All Youtube videos together</p>
+                                <h2 className="sec_use_text poppins-bold m-0">Why is Saqima useful?</h2>
 
                             </div>
 
-                            <div class="pb-4">
+                            <div className="pb-5">
 
-                                <p class="m-0 poppins-light"><i class="fa-solid fa-check chakemark"></i>In order</p>
-
-                            </div>
-
-                            <div class="pb-4">
-
-                                <p class="m-0 poppins-light"><i class="fa-solid fa-check chakemark"></i>To find out how much study you have left</p>
+                                <p className="m-0 poppins-extralight">Learn visually by watching our videos, understand the lesson by doing questions, and assess your skills by taking tests!</p>
 
                             </div>
 
-                            <div class="pb-5">
+                            <div className="pb-4">
 
-                                <p class="m-0 poppins-light"><i class="fa-solid fa-check chakemark"></i>If you don't understand, start with the lowest grade and better.</p>
+                                <p className="m-0 poppins-light"><i className="fa-solid fa-check chakemark"></i>All Youtube videos together</p>
+
+                            </div>
+
+                            <div className="pb-4">
+
+                                <p className="m-0 poppins-light"><i className="fa-solid fa-check chakemark"></i>In order</p>
+
+                            </div>
+
+                            <div className="pb-4">
+
+                                <p className="m-0 poppins-light"><i className="fa-solid fa-check chakemark"></i>To find out how much study you have left</p>
+
+                            </div>
+
+                            <div className="pb-5">
+
+                                <p className="m-0 poppins-light"><i className="fa-solid fa-check chakemark"></i>If you don't understand, start with the lowest grade and better.</p>
 
                             </div>
 
                             <div>
 
-                                <button class="sec_usetext_register_btn">Click here to register.</button>
+                                <button className="sec_usetext_register_btn">Click here to register.</button>
 
                             </div>
 
