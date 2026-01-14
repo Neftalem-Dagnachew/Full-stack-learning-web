27e2898bd4aecc8d49e06c388f31d231ebe9d85c
Neftalem <dneftalem@gmail.com>
2026-01-14 14:37:01 +0300

    finished register

--- Frontend/src/page/Login.jsx
+++ Frontend/src/page/Login.jsx
@@ -8,88 +8,88 @@ function Login() {
     return(
         <>
 
-            <div class="d-flex">
+            <div className="d-flex">
 
-                <div class="login_img_container">
+                <div className="login_img_container">
 
                     <div>
 
-                        <img class="position-fixed" src="IMG/auth.png" alt="" />
+                        <img className="position-fixed" src="IMG/auth.png" alt="" />
                         
                     </div>
 
                 </div>
 
-                <div class="all_login_container">
+                <div className="all_login_container">
 
-                    <div class="all_login_form_container">
+                    <div className="all_login_form_container">
 
-                        <div class="pb-5">
+                        <div className="pb-5">
 
                             <Link to="/"><img src={Logo} alt="" /></Link>
 
                         </div>
 
-                        <form class="loginForm" id="loginForm">
+                        <form className="loginForm" id="loginForm">
 
-                            <div class="d-flex auth_both_text pb-5">
+                            <div className="d-flex auth_both_text pb-5">
 
                                 <div>
-                                    <h4 class="poppins-semibold text-light m-0">Sign-in</h4>
+                                    <h4 className="poppins-semibold text-light m-0">Sign-in</h4>
                                 </div>
 
                                 <div>
-                                    <p class="poppins-extralight text-light m-0">Create an Account</p>
+                                    <p className="poppins-extralight text-light m-0">Create an Account</p>
                                 </div>
 
                             </div>
 
-                            <div class="input-wrapper">
+                            <div className="input-wrapper">
 
-                                <i class="fa-regular fa-user"></i>
+                                <i className="fa-regular fa-user"></i>
                                 <input type="email" name="email" placeholder="Email Address" required />
 
                             </div>
 
-                            <div class="input-wrapper">
+                            <div className="input-wrapper">
 
-                                <i class="fa-solid fa-lock"></i>
+                                <i className="fa-solid fa-lock"></i>
                                 <input type="password" name="password" placeholder="Password" required />
 
                             </div>
 
-                            <div class="d-flex forgetpassword_text pb-3">
+                            <div className="d-flex forgetpassword_text pb-3">
 
-                                <div class="remember-me">
+                                <div className="remember-me">
 
                                     <label>
-                                        <input class="checkbox" type="checkbox" />
-                                        <span class="checkmark poppins-extralight text-light">Remember Me</span>
+                                        <input className="checkbox" type="checkbox" />
+                                        <span className="checkmark poppins-extralight text-light">Remember Me</span>
                                     </label>
 
                                 </div>
 
                                 <div>
 
-                                    <p class="poppins-extralight text-light m-0">Forget password?</p>
+                                    <p className="poppins-extralight text-light m-0">Forget password?</p>
 
                                 </div>
 
                             </div>
 
-                            <div class="pb-3">
+                            <div className="pb-3">
 
-                                <button class="login_btn" type="submit">Log in</button>
+                                <button className="login_btn" type="submit">Log in</button>
 
-                                <div class="d-flex align-items-center">
-                                <hr class="w-50 text-light" /><p class="m-0 text-white-50 ps-2 pe-2 poppins-extralight">OR</p><hr class="w-50 text-light" />
+                                <div className="d-flex align-items-center">
+                                <hr className="w-50 text-light" /><p className="m-0 text-white-50 ps-2 pe-2 poppins-extralight">OR</p><hr className="w-50 text-light" />
                                 </div>
 
                             </div>
 
-                            <div class="d-flex justify-content-center">
+                            <div className="d-flex justify-content-center">
 
-                                <p class="text-light poppins-extralight">Terms of Service and Privacy Policy</p>
+                                <p className="text-light poppins-extralight">Terms of Service and Privacy Policy</p>
 
                             </div>
 
--- Frontend/src/page/Register.jsx
+++ Frontend/src/page/Register.jsx
@@ -1,9 +1,121 @@
+import "./style/auth.css"
+import Logo from "../assets/Navbar-img/Main-Logo.png"
+import { Link } from "react-router-dom";
 
 function Register() {
 
     return(
         <>
+            <div className="d-flex">
 
+            <div className="login_img_container">
+
+                <div>
+
+                    <img className="position-fixed" src="IMG/auth.png" alt="" />
+                    
+                </div>
+
+            </div>
+
+            <div className="all_register_container">
+
+                <div className="register_form_container">
+
+                    <div className="pb-5">
+
+                        <Link to="/"><img src={Logo} alt="" /></Link>
+
+                    </div>
+
+                    <form className="RegisterForm" id="registerForm">
+
+                        <div className="d-flex rigister_creat_text pb-5">
+
+                            <div>
+                                <h4 className="poppins-semibold text-light m-0">Create an Account</h4>
+                            </div>
+
+                            <div>
+                                <p className="poppins-extralight text-light m-0">Sign-in</p>
+                            </div>
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Email</p>
+                            <input type="email" name="email" required />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Password</p>
+                            <input type="password" name="password" required />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">First Name</p>
+                            <input type="text" name="first_name" required />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Last Name</p>
+                            <input type="text" name="last_name" required />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Nickname</p>
+                            <input type="text" name="nickname" />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Phone Number</p>
+                            <input type="text" name="phone_numer" required />
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-semibold text-light m-0 input_text">Identity</p>
+                            <select className="identity_section" id="identity" name="identity">
+                                <option value="">----</option>
+                                <option value="student">Student</option>
+                                <option value="parent">Parent / Family</option>
+                                <option value="teacher">Teacher</option>
+                            </select>
+
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <p className="poppins-extralight text-light m-0 input_text">By creating an account you are agreeing to the Terms of Service and Privacy Policy.</p>
+
+                        </div>
+
+                        <div className="input-rigister">
+
+                            <button className="login_btn" type="submit">Create an Account</button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
         </>
     )
 }
