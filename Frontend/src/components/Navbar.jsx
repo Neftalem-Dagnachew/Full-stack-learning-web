c2bc36e8c59889abf65685dbadb6f63a79317770
Neftalem <dneftalem@gmail.com>
2026-01-30 17:35:16 +0300

    add manu links

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -95,7 +95,7 @@ function Navbar() {
             <div className="d-flex gap-3 align-items-center">
               <i className="fa-solid fa-cart-arrow-down side_cart"></i>
 
-              <div className="profile_container position-relative">
+              <div className="profile_container">
 
                 <div className="profile_btn gap-2 d-flex align-items-center justify-content-center">
 
@@ -103,10 +103,12 @@ function Navbar() {
 
                   <img className="btn_profileImg" src={profileImg} alt="" />
 
-                  <div className="profile_manu">
-                    1234
-                  </div>
+                </div>
 
+                <div className="profile_manu">
+                  <Link className="profile_links" to="/dashboard">Dashboard</Link>
+                  <Link className="profile_links" to="/settings">Settings</Link>
+                  <button className="profile_links" onClick={logout}>Logout</button>
                 </div>
 
               </div>
