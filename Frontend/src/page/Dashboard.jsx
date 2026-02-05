2238cb9b61506302f557a70ee38c174c667d6b3c
Neftalem <dneftalem@gmail.com>
2026-02-05 17:07:00 +0300

    feat: add new img and box-shadow

--- Frontend/src/page/Dashboard.jsx
+++ Frontend/src/page/Dashboard.jsx
@@ -2,13 +2,21 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import "../page/style/Dashboard.css"
 
+import proile from "../assets/Navbar-img/profile.png"
+
 function Dashboard() {
     const { user } = useContext(AuthContext);
 
     return(
         <>
-            <h1>Dashboard</h1>
-            <p className="mt-5">Welcome, {user.first_name}</p>
+            <div>
+                <div className="d-flex">
+                    <div className="header_img_container d-flex justify-content-center align-items-center">
+                        <img className="profile_img" src={proile}  />
+                    </div>
+                    <p className="mt-5">Welcome, {user.first_name}</p>
+                </div>
+            </div>
         </>
     )
 }
