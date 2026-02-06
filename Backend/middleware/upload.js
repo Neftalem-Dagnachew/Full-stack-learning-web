9ab3c770a6a43e80cd2dfd352afa59c784a3f0b4
Neftalem <dneftalem@gmail.com>
2026-02-06 12:54:16 +0300

    feat: for photo add photo place and and name

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,2 +1,13 @@
 const multer = require('multer');
 const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cd) => {
+        cd(null, "uploads/")
+    },
+    filename: (req, file, cd) => {
+        // the file name is not repited use (timestamp + random number)
+        const uniqueSuffix = Date.now() + "_" + Math.random(Math.random() * 1E9);
+        cd(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
