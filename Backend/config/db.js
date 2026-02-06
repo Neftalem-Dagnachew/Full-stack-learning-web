d5011aaf5a4a4ef6d08ab747d54038ede163af8a
Neftalem <dneftalem@gmail.com>
2026-02-06 12:34:24 +0300

    refactor: replace database with new one

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "iconic_Neftalem3",
-    database: "main_learn_app"
+    database: "main_learn_app_v2"
 
 });
 
