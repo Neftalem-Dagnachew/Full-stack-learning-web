555c1cd01dea58f185ac74a119f604262c91ca42
Neftalem <dneftalem@gmail.com>
2026-02-07 23:37:08 +0300

    chore: add devs

--- Frontend/src/page/DashboardPages/EditProfile.jsx
+++ Frontend/src/page/DashboardPages/EditProfile.jsx
@@ -3,9 +3,19 @@ import "./style/EditProfile.css"
 function EditProfile() {
     return(
         <>
-            <div className="mt-4">
+            <div className="mt-4 gap-5 d-flex">
                 <div className="links_container">
                     <p className="m-0 profile_link">Profile</p>
+                    <div className="under_line"></div>
+                </div>
+
+                <div className="links_container">
+                    <p className="m-0 profile_link">Courses</p>
+                    <div className="under_line"></div>
+                </div>
+
+                <div className="links_container">
+                    <p className="m-0 profile_link">Achievements</p>
                     <div className="under_line"></div>
                 </div>
             </div>
