6aef1e5f1c003dd43de1d05c6b978829f488dde9
Neftalem <dneftalem@gmail.com>
2026-02-12 13:05:17 +0300

    feat: change profile form

--- Frontend/src/page/DashboardPages/FunctionalEditProfile.jsx
+++ Frontend/src/page/DashboardPages/FunctionalEditProfile.jsx
@@ -4,7 +4,51 @@ function FunctionalEditProfile() {
     return(
         <>
             <div className="all_container">
-                
+                <div className="edit-container">
+                    <h2>Edit "Details" Information</h2>
+
+                    {/* First Name */}
+                    <div className="form-group">
+                        <label>First name (required)</label>
+                        <input
+                        type="text"
+                        />
+                        <span className="privacy public">Public</span>
+                    </div>
+
+                    {/* Last Name */}
+                    <div className="form-group">
+                        <label>Last name (required)</label>
+                        <input
+                        type="text"
+                        />
+                        <div className="privacy-row">
+                        <span className="privacy public">Public</span>
+                        </div>
+                    </div>
+
+                    {/* Nickname */}
+                    <div className="form-group">
+                        <label>Nickname (required)</label>
+                        <input
+                        type="text"
+                        />
+                        <span className="privacy public">Public</span>
+                    </div>
+
+                    {/* Phone */}
+                    <div className="form-group">
+                        <label>Phone number (required)</label>
+                        <input
+                        type="text"
+                        />
+                        <div className="privacy-row">
+                        <span className="privacy private">Only Me</span>
+                        <button className="change-btn">Change</button>
+                        </div>
+                    </div>
+                    <button className="change_btn">Change</button>
+                </div>
             </div>
         </>
     )
