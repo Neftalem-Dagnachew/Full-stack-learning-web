adb706bdcc1f9a90754e3973e46629e984b82552
Neftalem <dneftalem@gmail.com>
2026-02-12 12:13:06 +0300

    style: import CSS file

--- Frontend/src/page/DashboardPages/FunctionalEditProfile.jsx
+++ Frontend/src/page/DashboardPages/FunctionalEditProfile.jsx
@@ -1,6 +1,11 @@
+import "../DashboardPages/style/FunctionalEditProfile.css"
 
 function FunctionalEditProfile() {
-
+    return(
+        <>
+            
+        </>
+    )
 }
 
 export default FunctionalEditProfile;
