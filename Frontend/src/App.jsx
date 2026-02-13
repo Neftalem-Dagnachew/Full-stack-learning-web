2c4042d0bc3491324eec8ac6e938a8a196277325
Neftalem <dneftalem@gmail.com>
2026-02-13 20:20:09 +0300

    create Route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -13,6 +13,7 @@ import Profile from "./page/DashboardPages/profile";
 import FunctionalEditProfile from "./page/DashboardPages/FunctionalEditProfile";
 import ChangeAvatar from "./page/DashboardPages/ChangeAvatar";
 import EditProfileLayout from "./layouts/EditProfileLayout";
+import ChangeCoverImag from "./page/DashboardPages/ChangeCoverImag";
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
           <Route path="members/:nickname/profile/edit" element={<EditProfileLayout />}>
             <Route index element={<FunctionalEditProfile />} />
             <Route path="change-avatar" element={<ChangeAvatar />} />
+            <Route path="change-cover-image" element={<ChangeCoverImag />} />
           </Route>
         </Route>
       </Routes>
