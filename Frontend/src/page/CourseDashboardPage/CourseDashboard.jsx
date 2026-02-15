8687615ed1b14ac7ac7cb05a64ba74337241e1c6
Neftalem <dneftalem@gmail.com>
2026-02-15 18:10:12 +0300

    style: add width, height and add new css file

---
+++ Frontend/src/page/CourseDashboardPage/CourseDashboard.jsx
@@ -0,0 +1,15 @@
+
+import "../CourseDashboardPage/style/CourseDashboard.css"
+
+function CourseDashboard() {
+    return(
+        <>
+            <div className="course_layout">
+                <div className="CourseDashboard_container">
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CourseDashboard;
