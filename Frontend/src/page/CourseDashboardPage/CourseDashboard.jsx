3f3dd9b38c75dd724fd8684e1cdda36b990a04be
Neftalem <dneftalem@gmail.com>
2026-02-15 20:47:40 +0300

    feat: add Select

--- Frontend/src/page/CourseDashboardPage/CourseDashboard.jsx
+++ Frontend/src/page/CourseDashboardPage/CourseDashboard.jsx
@@ -5,7 +5,19 @@ function CourseDashboard() {
     return(
         <>
             <div className="course_layout">
-                <div className="CourseDashboard_container">
+                <div className="CourseDashboard_container d-flex align-items-center justify-content-center">
+
+                    <div>
+                        <h3>Educational standards</h3>
+                        <select className="grade_select" name="" id="">
+                            <option value="">Grade choice</option>
+                            <option value="Grade 9">Grade 9</option>
+                            <option value="Grade 10">Grade 10</option>
+                            <option value="Grade 11">Grade 11</option>
+                            <option value="Grade 12">Grade 12</option>
+                        </select>
+                    </div>
+
                 </div>
             </div>
         </>
