edddfe909f993c5a2aa3fd52fbcf475ec78c0d4a
Neftalem <dneftalem@gmail.com>
2026-02-17 06:14:51 +0300

    style: new div and style

--- Frontend/src/page/CourseDashboardPage/CourseStart.jsx
+++ Frontend/src/page/CourseDashboardPage/CourseStart.jsx
@@ -4,7 +4,14 @@ import "../CourseDashboardPage/style/CourseStart.css"
 function CourseStart() {
     return(
         <>
-            <div className="all_course_start_container"></div>
+            <div className="all_course_start_container">
+                <div className="header_course_start d-flex align-items-center w-100 ">
+                    <div className="ps-5 text-white">
+                        <p className="m-0">9th grade</p>
+                        <h1 className="header_text_course">Grade 9 Maths Unit 9: Statistics and Probability</h1>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
