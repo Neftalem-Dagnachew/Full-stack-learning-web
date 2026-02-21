003dc78cfd718e6bf011aca66d7ec3af47b9d221
Neftalem <dneftalem@gmail.com>
2026-02-21 10:24:28 +0300

    feat: import global data

--- Frontend/src/page/CourseDashboardPage/CourseStart.jsx
+++ Frontend/src/page/CourseDashboardPage/CourseStart.jsx
@@ -1,19 +1,19 @@
 
 import "../CourseDashboardPage/style/CourseStart.css"
 
-import { AuthContext } from "../../context/AuthContext";
+import { CourseContext } from "../../context/CourseContext";
 import { useContext } from "react";
 
 function CourseStart() {
 
-    const { user } = useContext(AuthContext);
+    const { grade, setGrade, subject, setSubject, courses, loading, feachCourses } = useContext(CourseContext);
 
     return(
         <>
             <div className="all_course_start_container">
                 <div className="header_course_start d-flex align-items-center w-100 ">
                     <div className="ps-5 text-white">
-                        <p className="m-0">9th grade</p>
+                        <p>{courses.grade_level}</p>
                         <h1 className="header_text_course">Grade 9 Maths Unit 9: Statistics and Probability</h1>
                     </div>
                     <div className="getStart_container w-25">
@@ -29,6 +29,10 @@ function CourseStart() {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div>
+                    <h4>Course Content</h4>
+                    <h5></h5>
                 </div>
             </div>
         </>
