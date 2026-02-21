d4a61a5bdfb2ed2f1903d5872c58913eabe5dad5
Neftalem <dneftalem@gmail.com>
2026-02-21 12:27:37 +0300

    feat: add circle button

--- Frontend/src/page/CourseDashboardPage/CourseStart.jsx
+++ Frontend/src/page/CourseDashboardPage/CourseStart.jsx
@@ -43,8 +43,12 @@ function CourseStart() {
                 </div>
                 <div className="lessons_button_container d-flex flex-column gap-2 w-50">
                     <h4>Course Content</h4>
-                    <div className="w-100 all_lessons_button d-flex align-items-center">
-                        <Link className="lessons_button">{course.title}</Link> <i class="fa-solid fa-lock ps-3 lock_i"></i>
+                    <div className="w-100 all_lessons_button d-flex align-items-center justify-content-between">
+                        <div>
+                            <Link className="lessons_button">{course.title}</Link>
+                            <i class="fa-solid fa-lock ps-3 lock_i"></i>
+                        </div>
+                        <span className="status-circle"></span>
                     </div>
 
                 </div>
