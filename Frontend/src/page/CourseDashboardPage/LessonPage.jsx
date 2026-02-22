01b561583b816654946298f69b62282e01dbdae7
Neftalem <dneftalem@gmail.com>
2026-02-22 15:40:13 +0300

    style: style hr and add padding

--- Frontend/src/page/CourseDashboardPage/LessonPage.jsx
+++ Frontend/src/page/CourseDashboardPage/LessonPage.jsx
@@ -11,9 +11,10 @@ function LessonPage() {
     return (
         <div className="all_course_lesson_container">
             <div className="course_lesson_container ps-4 pt-5 w-25">
-                <div>
+                <div className="w-100 pe-4">
                     <button className="mb-4 backTo_course_btn">Back to course</button>
                     <h3>Grade 9 Biology Unit 6: Ecology</h3>
+                    <hr className="hr_complit"/>
                 </div> 
             </div>
         </div>
