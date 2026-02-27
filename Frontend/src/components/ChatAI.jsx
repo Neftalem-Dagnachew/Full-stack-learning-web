1719d8c365e3eb45fe847bf101a5140c21945406
Neftalem <dneftalem@gmail.com>
2026-02-27 19:21:34 +0300

    feat: new function and div

---
+++ Frontend/src/components/ChatAI.jsx
@@ -0,0 +1,13 @@
+
+
+function ChatComponent() {
+  return(
+    <>
+      <div className="all_container">
+        <div className="chat_component"></div>
+      </div>
+    </>
+  )
+}
+
+export default ChatComponent;
