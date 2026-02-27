c9039401d528e744dfccd2e7888fef859100d1ee
Neftalem <dneftalem@gmail.com>
2026-02-27 19:57:49 +0300

    feat: creat new file

--- Frontend/src/components/ChatAI.jsx
+++ Frontend/src/components/ChatAI.jsx
@@ -1,10 +1,24 @@
 
+import "../components/style/ChatComponent.css";
+
+import { useState } from "react";
 
 function ChatComponent() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return(
     <>
-      <div className="all_container">
-        <div className="chat_component"></div>
+      <div className="all_container d-flex">
+        <div className="chat_component w-50 mt-5 d-flex gap-5">
+          {isOpen && (
+            <div className="chat_window_container">
+              we
+            </div>
+          )}
+
+          <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "Close Chat" : "Open Chat"}</button>
+        </div>
       </div>
     </>
   )
